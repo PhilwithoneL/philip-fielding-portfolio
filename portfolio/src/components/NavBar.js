f6df25f888aa8e189bf3447ae0c2b2a33781a678
PhilwithoneL <philip.r.fielding@gmail.com>
2021-09-23 11:07:22 +0100

    Navigation bar
    - Added a hover animation on the navigation links

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -4,11 +4,11 @@ const navBar = () => {
     return (
         <div>
             <div className = "nav-bar row">
-                <div className = "link col m3 s1 center-div link-mobile"></div>
-                <div className ="link col m2 s4 center-div"><p>About</p></div>
-                <div className ="link col m2 s4 center-div"><p>Projects</p></div>
-                <div className ="link col m2 s4 center-div"><p>Contact</p></div>
-                <div className = "link col m3 s1 center-div link-mobile"></div>
+                <div className = "col m3 s1 center-div link-mobile"></div>
+                <div className ="link col m2 s4 center-div"><p className = "link-text">About</p></div>
+                <div className ="link col m2 s4 center-div"><p className = "link-text">Projects</p></div>
+                <div className ="link col m2 s4 center-div"><p className = "link-text">Contact</p></div>
+                <div className = "col m3 s1 center-div link-mobile"></div>
             </div>
         </div>
     )
