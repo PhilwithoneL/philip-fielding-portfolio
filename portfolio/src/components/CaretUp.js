2e73696074ab124edb6a28c4f3f237670030a97a
PhilwithoneL <philip.r.fielding@gmail.com>
2021-10-02 10:28:05 +0100

    Caret up
    - Created caret icon
    
    - Positioned to bottom center
    
    - Added an animation

--- portfolio/src/components/CaretUp.js
+++ portfolio/src/components/CaretUp.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { AiFillCaretUp } from 'react-icons/ai';
+
 
 const CaretUp = () => {
+
     return (
         <div>
-            
+            <div className ="caret-up-container">
+                <div className ="caret-position">
+                    <AiFillCaretUp className = "caret-icon"/></div>
+                </div>            
         </div>
     )
 }
