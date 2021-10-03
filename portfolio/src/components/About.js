28e4a5220d702043d17550fcee04619622a80666
PhilwithoneL <philip.r.fielding@gmail.com>
2021-10-03 15:29:03 +0100

    About page
    - Added tech I've to about section

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -8,7 +8,10 @@ const About = () => {
             <div className= "section about-section">
                 <div className = "about">
                     <h1 className = "about-header">Who am I?</h1>
-                    <h4>I'm an aspiring web developer from Manchester.</h4>
+                    <h4>I'm a web developer from the northern powerhouse, <h className = "about-important">Manchester</h>.</h4>
+
+                    <h4>The tech i've used include <h className = "about-important tech-icon-html">HTML</h>, <h className = "about-important tech-icon-css">CSS</h>, <h className = "about-important tech-icon-js">Javascript</h>, <h className = "about-important tech-icon-react">React</h>, <h className = "about-important tech-icon-material">Materialize</h>, <h className = "about-important tech-icon-tailwind">Tailwind</h>.</h4>
+
                 </div>
             </div>
 
--- portfolio/src/components/InfoLinks.js
+++ portfolio/src/components/InfoLinks.js
@@ -13,7 +13,7 @@ const InfoLinks = () => {
                 <div className = "col m6 row">
 
                     <div className = "col m4 center-div"><VscGithub className = "git-icon" /></div>
-                    <div className = "col m4 center-div"><div className ="cv"><b>Get CV</b></div></div>
+                    <div className = "col m4 center-div "><div className ="cv"><b>Get CV</b></div></div>
                     <div className = "col m4 center-div"><SiLinkedin className ="linked-icon" /></div>
 
                 </div>
