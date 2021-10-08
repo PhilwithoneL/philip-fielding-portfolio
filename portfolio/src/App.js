8560953450ce78c3b44b5399ed0e0190a274716a
philipfielding <philip.r.fieldingdev@gmail.com>
2021-10-08 07:29:42 +0100

    Projects
    - Created the greneral look for a project section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -25,8 +25,6 @@ function App() {
       <About />
       <Projects />
       
-
-      
     </div>
   );
 }
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import Project from './Project';
+import Img1 from '../img/reviewme.jpg';
 
 const Projects = () => {
+
     return (
         <div>
             <div className = "project-section">
-
+                <h2 className = "project-header">Some projects I've done</h2>
                 <div className = "row">
-
-                    <div className = "col m6 s12"><Project /></div>
-                    <div className = "col m6 s12"><Project /></div>
                     
+                    <div className = "col m6 s12">
+                    <div className = "project-box">
+                        <div className = "project-img-container">
+                            <img className = "project-img" src = {Img1} style={{ filter: "grayscale(100%)" }}></img>
+                            <div className = "project-name-container">
+                                <div className = "project-name">
+                                    <div className ="project-name-text center-div">Reviewme</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className = "project-info">
+                            <h5><b>Review.me</b></h5>
+                        </div>
+                    </div>
+
+                    </div>
+                    <div className = "col m6 s12"></div>
                 </div>
 
             </div>
