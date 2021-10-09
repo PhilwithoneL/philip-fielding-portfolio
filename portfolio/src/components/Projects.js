c9fecf09eed5528f33af034dc70dbccef1bfa843
philipfielding <philip.r.fieldingdev@gmail.com>
2021-10-09 10:58:52 +0100

    Projects
    - Project responsive boxes for project info
    
    - Added a little info for the review.me project

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -6,26 +6,62 @@ const Projects = () => {
     return (
         <div>
             <div className = "project-section">
-                <h2 className = "project-header">Some projects I've done</h2>
+                <h2 className = "project-header">Projects</h2>
                 <div className = "row">
                     
                     <div className = "col m6 s12">
-                    <div className = "project-box">
-                        <div className = "project-img-container">
-                            <img className = "project-img" src = {Img1} style={{ filter: "grayscale(100%)" }}></img>
-                            <div className = "project-name-container">
-                                <div className = "project-name">
-                                    <div className ="project-name-text center-div">Reviewme</div>
+
+                        <div className = "project-box-left" >
+                            <div className = "project-img-container">
+                                <img className = "project-img" src = {Img1} style={{ filter: "grayscale(100%)" }}></img>
+
+                                <div className = "project-links center-div">
+                                    <div className ="btn project-link-btn black-text waves-effect waves-light btn">Github</div>
+                                    <div className ="btn project-link-btn black-text waves-effect waves-light btn">Site</div>
+                                </div>
+            
+                            </div>
+                            <div className = "project-info">
+                                <h5><b>Review.me</b></h5>
+                                <p>A web page using The Movie Database Api, getting the trending movies and most up to date movie trailers.</p>
+                                <div className = "tech-div center-div">
+                                    <p className = "tech-div-text">Javascript</p>
+                                    <p className = "tech-div-text">Scss</p>
+                                    <p className = "tech-div-text">Html</p>
+                                    <p className = "tech-div-text">Bootstrap</p>
                                 </div>
                             </div>
                         </div>
-                        <div className = "project-info">
-                            <h5><b>Review.me</b></h5>
-                        </div>
-                    </div>
+
 
                     </div>
-                    <div className = "col m6 s12"></div>
+
+                    <div className = "col m6 s12 center-div">
+
+                    <div className = "project-box-right" >
+                            <div className = "project-img-container">
+                                <img className = "project-img" src = {Img1} style={{ filter: "grayscale(100%)" }}></img>
+
+                                <div className = "project-links center-div">
+                                    <div className ="btn project-link-btn black-text waves-effect waves-light btn">Github</div>
+                                    <div className ="btn project-link-btn black-text waves-effect waves-light btn">Site</div>
+                                </div>
+            
+                            </div>
+                            <div className = "project-info">
+                                <h5><b>Review.me</b></h5>
+                                <p>A web page using The Movie Database Api, getting the trending movies and most up to date movie trailers.</p>
+                                <div className = "tech-div center-div">
+                                    <p className = "tech-div-text">Javascript</p>
+                                    <p className = "tech-div-text">Scss</p>
+                                    <p className = "tech-div-text">Html</p>
+                                    <p className = "tech-div-text">Bootstrap</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
 
             </div>
