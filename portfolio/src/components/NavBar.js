c541a9010b77c46aad72140e936f493105f66d4c
philipfielding <philip.r.fieldingdev@gmail.com>
2021-10-28 15:18:15 +0100

    Main Nav
    Added styles so that the nav bar pops in on scroll

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const navBar = () => {
+const NavBar = () => {
+
+    const [offset, setOffset] = useState(0);
+
+
+
+    useEffect(() => {
+        window.onscroll = () => {
+        setOffset(Math.round(window.pageYOffset / 50) * 50 )
+        }
+    }, []);
+
+    console.log(offset);
+   
+  
+        switch(offset) {
+
+            case 100 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 0%)";
+                console.log("hit 20")
+                break;
+            case 150 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 10%)";
+                break;
+            case 200 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 20%)";
+                break;
+            case 250 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 30%)";
+                break;
+            case 300 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 40%)";
+                break;
+            case 350 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 50%)";
+                break;
+            case 400 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 60%)";
+                break;
+            case 450 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 70%)";
+                break;
+            case 500 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 80%";
+                break;
+            case 550 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 90%)";
+                break;
+            case 600 :
+                document.getElementById("navBar").style.backgroundColor = "rgb(30, 32, 36, 100%)";
+                break;
+        }
+
+
+
     return (
         <div>
-            <div className = "nav-bar row">
+            <div className = "nav-bar row" id ="navBar">
                 <div className = "col m3 s1 center-div link-mobile"></div>
                 <div className ="link col m2 s4 center-div"><p className = "link-text">About</p></div>
                 <div className ="link col m2 s4 center-div"><p className = "link-text">Projects</p></div>
@@ -14,4 +68,4 @@ const navBar = () => {
     )
 }
 
-export default navBar
+export default NavBar
