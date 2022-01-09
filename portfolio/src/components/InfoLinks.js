e1356bfee12aa8ae730da77fb085061f4c5ba78b
Philip Fielding <philip.r.fieldingdev@gmail.com>
2022-01-09 13:14:09 +0000

    CV link
    - Added link to CV to PDF document

--- portfolio/src/components/InfoLinks.js
+++ portfolio/src/components/InfoLinks.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { VscGithub } from 'react-icons/vsc';
 import { RiLinkedinBoxLine } from 'react-icons/ri';
 import { CgNotes } from 'react-icons/cg';
+import CV from '../docs/cv.pdf';
+import Img4 from '../img/the-recipe-app-img.jpg';
 
 const InfoLinks = () => {
     return (
@@ -14,8 +16,8 @@ const InfoLinks = () => {
                 <div className = "col m6 s8 row center-div">
 
                     <a className = "col center-div git-icon" href='https://github.com/PhilwithoneL'><VscGithub /></a>
-                    <a className = "col center-div cv"><CgNotes /></a>
-                    <a className = "col center-div linked-icon"><RiLinkedinBoxLine /></a>
+                    <a className = "col center-div cv" href = {CV} target="_blank"><CgNotes /></a>
+                    <a className = "col center-div linked-icon" href="https://www.linkedin.com/in/philip-fielding-39b39120a/"><RiLinkedinBoxLine /></a>
 
                 </div>
 
